dc85ea08fcd837ea5b5a9db0e03df1637be6d81b
Heath Farrow <heath.farrow@gmail.com>
2014-03-27 12:01:39 -0700

    Added todo to core testrunner to add a "fail/crash on assert" function

--- modules/fscore/tests/src/runner.cpp
+++ modules/fscore/tests/src/runner.cpp
@@ -10,6 +10,11 @@ class GlobalFixture
 public:
     GlobalFixture()
     {
+        // TODO: need a way to make asserts fail the unit tests because an automated
+        // build would hang on the assert until it is skipped.
+        // Maybe a function that would configure assert to crash on failure
+        // ie: fs::setCrashOnAssert(true);
+        //fs::setIgnoreAsserts(true);
     }
 
     ~GlobalFixture()

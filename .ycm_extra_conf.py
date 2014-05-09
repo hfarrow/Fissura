fc7446b12e137fe474b9ace45d45b9ca70149bf7
Heath Farrow <heath.farrow@disney.com>
2014-05-09 10:59:52 -0700

    Cleaned up .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -44,62 +44,22 @@ flags = [
 '-fexceptions',
 '-DNDEBUG',
 '-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
 '-std=c++11',
 #'-stdlib=libc++',
 #'-m64',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-#'-isystem',
-#'../BoostParts',
-#'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-#'/System/Library/Frameworks/Python.framework/Headers',
-#'-isystem',
-#'../llvm/include',
-#'-isystem',
-#'../llvm/tools/clang/include',
-'-I',
-'modules/fscore/include',
-'-I',
-'modules/fscore/tests/include',
-'-I',
-'modules/fsgame/include',
-'-I',
-'modules/fsgame/tests/include',
-'-I',
-'modules/fstest/include',
-'-I',
-'external/build/include/SDL2',
-'-I',
-'external/build/include/tinyxml',
-'-isystem',
-'/usr/local/include/boost',
-'-isystem',
-'/usr/local/boost_1_54_0',
-'-isystem',
-'/usr/include/',
-'-isystem',
-'/usr/lib/c++/v1'
-#'-I',
-#'-isystem',
-#'./tests/gmock/gtest',
-#'-isystem',
-#'./tests/gmock/gtest/include',
-#'-isystem',
-#'./tests/gmock',
-#'-isystem',
-#'./tests/gmock/include'
+'-I', 'modules/fscore/include',
+'-I', 'modules/fscore/tests/include',
+'-I', 'modules/fsgame/include',
+'-I', 'modules/fsgame/tests/include',
+'-I', 'modules/fstest/include',
+'-I', 'external/build/include/SDL2',
+'-I', 'external/build/include/tinyxml',
+'-isystem', '/usr/local/include/boost',
+'-isystem', '/usr/local/boost_1_54_0',
+'-isystem', '/usr/include/',
+'-isystem', '/usr/lib/c++/v1'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
